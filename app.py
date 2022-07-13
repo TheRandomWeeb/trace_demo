c42b9b0032899d485bd633179e6fc1d11b60c1bf
TheRandomWeeb <42524096+TheRandomWeeb@users.noreply.github.com>
2022-07-13 12:55:33 -0700

    Made PSQL compatible

--- app.py
+++ app.py
@@ -1,40 +1,28 @@
-from flask import Flask, redirect
-from flask import render_template
-from flask import request
+from flask import Flask, redirect, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-log_num = 0
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://cwkuktrakwieja:016c01c9c5a823a9556c63f0660d9f805aa7b614ab525daf6ad30dead62b2fbb@ec2-3-219-229-143.compute-1.amazonaws.com:5432/d1scl9b4lktjgj'
+
+db = SQLAlchemy(app)
 
 
-# Loading Data
-def grabData():
-    with open('SaveData.txt', 'r') as f:
-        global log_num
-        log_num = int(f.readline(1))
-
-
-def storeData():
-    with open('SaveData.txt', 'w') as f:
-        f.write(str(log_num) + '\n')
-
-
-def writeNew(data):
-    with open('IPs.txt', 'a') as f:
-        f.write(data + '\n')
-
-
-grabData()
+class IPs(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    ip = db.Column(db.Text)
 
 
 # Server-Side
 @app.route("/", methods=["GET"])
 def home():
-    global log_num
-    log_num += 1
-    storeData()
     x_ip = request.environ.get('HTTP_X_FORWARDED_FOR', request.remote_addr)
-    writeNew(f'IP {log_num}: {x_ip}')
-    # return render_template('page.html')
+    ipTable = IPs(ip=x_ip)
+
+    db.session.add(ipTable)
+    db.session.commit()
+
     return redirect("https://www.youtube.com/", code=302)
 
 
